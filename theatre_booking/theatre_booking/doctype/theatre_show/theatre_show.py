3807b31b1fd6f1f0b6d2e8675499a0367370107e
KulkarniPratham2004 <ikulkarniprathmesh@gmail.com>
2026-08-09 12:16:50 +0530

    Add theatre show date validation

--- theatre_booking/theatre_booking/doctype/theatre_show/theatre_show.py
+++ theatre_booking/theatre_booking/doctype/theatre_show/theatre_show.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2026, Prathamesh Kulkarni and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate, today
 
 
 class TheatreShow(Document):
-	pass
+
+    def validate(self):
+        self.validate_show_date()
+
+    def validate_show_date(self):
+        if self.show_date and getdate(self.show_date) < getdate(today()):
+            frappe.throw("Show Date cannot be in the past.") 
