b4ac1e266cba3d5de94eb90878b0642f54aa42c9
KulkarniPratham2004 <ikulkarniprathmesh@gmail.com>
2026-08-10 08:49:19 +0530

    Add theatre show seat validation

--- theatre_booking/theatre_booking/doctype/theatre_show/theatre_show.py
+++ theatre_booking/theatre_booking/doctype/theatre_show/theatre_show.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2026, Prathamesh Kulkarni and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 from frappe.utils import getdate, today
@@ -10,7 +7,12 @@ class TheatreShow(Document):
 
     def validate(self):
         self.validate_show_date()
+        self.validate_total_seats()
 
     def validate_show_date(self):
         if self.show_date and getdate(self.show_date) < getdate(today()):
-            frappe.throw("Show Date cannot be in the past.") 
+            frappe.throw("Show Date cannot be in the past.")
+
+    def validate_total_seats(self):
+        if self.total_seats is not None and self.total_seats <= 0:
+            frappe.throw("Total Seats must be greater than 0.")
